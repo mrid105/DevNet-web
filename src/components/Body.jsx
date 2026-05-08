0d766ec121760460d36f78644a2037c5cce3dd45
Mridul <mridul1052003@gmail.com>
2026-05-08 15:33:29 -0400

    Proper redirection and login logic

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,7 +1,37 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+
 const Body = () => {
+  const navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
+  const fetchUser = async () => {
+    if (userData) return;
+    try {
+      const res = await axios.get(
+        BASE_URL + "/profile/view",
+        {},
+        { withCredentials: true },
+      );
+      dispatch(addUser(res.data));
+    } catch (error) {
+      if (error.status === 401) {
+        navigate("/login");
+      }
+      console.error("Error fetching user data:", error);
+    }
+  };
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    fetchUser();
+  }, []);
   return (
     <>
       <NavBar />
